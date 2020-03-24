12caec5fc3547856af521ea71630fdd6d36d02bf
siascone <spencer.iascone@gmail.com>
2020-03-24 10:04:39 -0700

    map notes to tone rows, only grabbing first column/

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 const synth = new Tone.Synth().toMaster();
 
+// Tones e f# g# b c#
+document.querySelector('.tone-row1').addEventListener('click', async () => {
+    await Tone.start();
+    synth.triggerAttackRelease('C#4', '8n');
+});
 
-document.querySelector('button').addEventListener('click', async () => {
-    await Tone.start()
-synth.triggerAttackRelease('C4', '8n')
-    console.log('audio is ready')
-})
+document.querySelector('.tone-row2').addEventListener('click', async () => {
+    await Tone.start();
+    synth.triggerAttackRelease('B3', '8n');
+});
+
+document.querySelector('.tone-row3').addEventListener('click', async () => {
+    await Tone.start();
+    synth.triggerAttackRelease('G#3', '8n');
+});
+
+document.querySelector('.tone-row4').addEventListener('click', async () => {
+    await Tone.start();
+    synth.triggerAttackRelease('F#3', '8n');
+});
+
+document.querySelector('.tone-row5').addEventListener('click', async () => {
+    await Tone.start();
+    synth.triggerAttackRelease('E3', '8n');
+});
+
+// 
+
+// Drums
