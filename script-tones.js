7c5b71656f74ea3e5926cd6a52604b5747d3ab29
siascone <spencer.iascone@gmail.com>
2020-03-24 12:18:18 -0700

    edit tones, woodwind/ocarina

--- script-tones.js
+++ script-tones.js
@@ -1,4 +1,14 @@
-const synth = new Tone.Synth().toMaster();
+const synth = new Tone.Synth({
+    oscillator: {
+        type: 'sine'
+    },
+    envelope: {
+        attack: 0.01,
+        decay: 0.2,
+        sustain: 0.4,
+        release: 0.5
+    }
+}).toMaster();
 
 // Tones e f# g# b c#
 const cSharp4 = document.querySelectorAll('.tone-row1');
@@ -10,30 +20,30 @@ const e3 = document.querySelectorAll('.tone-row5');
 for (let i = 0; i < cSharp4.length; i++) {
     cSharp4[i].addEventListener('click', async () => {
         await Tone.start();
-        synth.triggerAttackRelease('C#4', '8n');
+        synth.triggerAttackRelease('C#4', '8n').volume.value = 3;
     });
 }
 for (let i = 0; i < b3.length; i++) {
     b3[i].addEventListener('click', async () => {
         await Tone.start();
-        synth.triggerAttackRelease('B3', '8n');
+        synth.triggerAttackRelease('B3', '8n').volume.value = 3;
     });
 }
 for (let i = 0; i < gSharp3.length; i++) {
     gSharp3[i].addEventListener('click', async () => {
         await Tone.start();
-        synth.triggerAttackRelease('G#3', '8n');
+        synth.triggerAttackRelease('G#3', '8n').volume.value = 3;
     });
 }
 for (let i = 0; i < fSharp3.length; i++) {
     fSharp3[i].addEventListener('click', async () => {
         await Tone.start();
-        synth.triggerAttackRelease('F#3', '8n');
+        synth.triggerAttackRelease('F#3', '8n').volume.value = 3;
     });
 }
 for (let i = 0; i < e3.length; i++) {
     e3[i].addEventListener('click', async () => {
         await Tone.start();
-        synth.triggerAttackRelease('E3', '8n');
+        synth.triggerAttackRelease('E3', '8n').volume.value = 3;
     });
 }
